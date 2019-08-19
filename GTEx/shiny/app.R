7f2c7272b405dc72dde1b3dc51c1a98d0485e810
Matthew Berginski <matthew.berginski@gmail.com>
2019-08-19 13:08:00 -0400

    variable name change and removing debug print

--- GTEx/shiny/app.R
+++ GTEx/shiny/app.R
@@ -6,7 +6,7 @@ library(here)
 library(BerginskiRMisc)
 
 kinase_percentiles = read_csv(here('GTEx/GTex_kinase_percentiles.csv'))
-dark_kinases_percentiles = kinase_percentiles %>% 
+dark_kinase_percentiles = kinase_percentiles %>% 
   filter(class == "Dark") %>% 
   #This bit of code adds a column for the positional index of the ranked kinase
   #percentile, will be used for plotting purposes
@@ -21,12 +21,12 @@ dark_kinases_percentiles = kinase_percentiles %>%
   mutate(organ_percentile_index = 1:n()) %>%
   ungroup()
 
-dark_kinase_order = dark_kinases_percentiles %>%
+dark_kinase_order = dark_kinase_percentiles %>%
   group_by(symbol) %>%
   summarise(mean_percentile = mean(kinase_percentile)) %>% 
   arrange(desc(mean_percentile))
 
-dark_kinase_order_by_system = dark_kinases_percentiles %>%
+dark_kinase_order_by_system = dark_kinase_percentiles %>%
   group_by(tissue_type) %>%
   summarise(mean_percentile = mean(kinase_percentile)) %>% 
   arrange(desc(mean_percentile))
@@ -115,7 +115,7 @@ server <- function(input, output) {
   #Kinase Searching Functions
   #############################################################################
   this_kinase_selection <- reactive({
-    dark_kinases_percentiles %>%
+    dark_kinase_percentiles %>%
       filter(symbol == input$kinase, 
              kinase_percentile >= input$min_percentile) %>% 
       arrange(desc(kinase_percentile), desc(TPM))
@@ -132,10 +132,10 @@ server <- function(input, output) {
   })
 
   output$kinase_percentile_dist <- renderPlot({
-    full_kinase_data = dark_kinases_percentiles %>%
+    full_kinase_data = dark_kinase_percentiles %>%
       filter(symbol == input$kinase)
     
-    ggplot(dark_kinases_percentiles, aes(x=percentile_index,y=kinase_percentile, group=symbol)) + 
+    ggplot(dark_kinase_percentiles, aes(x=percentile_index,y=kinase_percentile, group=symbol)) + 
       geom_line(alpha=0.05) + 
       ylim(c(0,100)) + 
       geom_line(data=full_kinase_data, aes(x=percentile_index,y=kinase_percentile)) +
@@ -160,14 +160,13 @@ server <- function(input, output) {
   #Organ Searching Functions
   #############################################################################
   this_organ_selection <- reactive({
-    dark_kinases_percentiles %>%
+    dark_kinase_percentiles %>%
       filter(tissue_type == input$tissue_type, 
              kinase_percentile >= input$min_percentile_organ) %>% 
       arrange(desc(kinase_percentile), desc(TPM))
   })
   
   output$anato_male_by_organ <- renderPlot({
-    print(this_organ_selection())
     gganatogram(data=hgMale_key %>% filter(organ %in% this_organ_selection()$organ),
                 sex = 'Male') + theme_void()
   })
@@ -178,10 +177,10 @@ server <- function(input, output) {
   })
   
   output$organ_percentile_dist <- renderPlot({
-    full_organ_data = dark_kinases_percentiles %>%
+    full_organ_data = dark_kinase_percentiles %>%
       filter(tissue_type == input$tissue_type)
     
-    ggplot(dark_kinases_percentiles, aes(x=organ_percentile_index,y=kinase_percentile,group=tissue_type)) + 
+    ggplot(dark_kinase_percentiles, aes(x=organ_percentile_index,y=kinase_percentile,group=tissue_type)) + 
       geom_line(alpha=0.05) + 
       ylim(c(0,100)) + 
       geom_line(data=full_organ_data, aes(x=organ_percentile_index,y=kinase_percentile)) +
